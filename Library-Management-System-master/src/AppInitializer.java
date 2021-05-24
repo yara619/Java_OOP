a485d248ce06cb1aa4f6dd44944f17d9f2941f62
Mehmood Yar Baig <48985550+yara619@users.noreply.github.com>
2021-05-24 13:37:19 +0600

    Add files via upload

---
+++ Library-Management-System-master/src/AppInitializer.java
@@ -0,0 +1,26 @@
+import Database.DBConnection;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class AppInitializer extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+        try {
+            DBConnection.getInstance().getConnection().close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/View/HomeFormView.fxml"))));
+        primaryStage.show();
+    }
+}
